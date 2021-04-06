211f646427ab08ac099a1093f9685c0c1f86bc4f
Rakshithamadappa <rakshita@targetclose.com>
2021-04-06 20:42:13 +0530

    Module 2 changes

---
+++ src/components/Todo.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+import Backdrop from "./Backdrop";
+import Model from "./Model";
+
+function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>
+          DELETE
+        </button>
+      </div>
+      {modalIsOpen && (
+        <Model onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
+    </div>
+  );
+}
+
+export default Todo;
